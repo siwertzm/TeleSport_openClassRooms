f976a55ccd005f0e447880d5147776d677f86726
Marius <siwertzm@gmail.com>
2025-09-26 13:22:57 +0200

    travail home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,24 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
+  public nbJos$: Observable<number> = of(0);
+  public nbCountries$: Observable<number> = of(0);
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.nbCountries$ = this.olympics$.pipe(
+      map((data) => (data ? data.length : 0))
+    );
+    this.nbJos$ = this.olympics$.pipe(
+      map(data => {
+        if (!data) return 0;
+        const allYears = data.flatMap((country: any) => 
+          country.participations.map((p: any) => p.year)
+        );
+        return [...new Set(allYears)].length;
+      })
+    );
   }
 }
